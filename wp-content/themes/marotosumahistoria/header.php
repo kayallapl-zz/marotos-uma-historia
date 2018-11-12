6cc26f5a4a779d53eab08ed9cb7eb9b397e46fd7
Kayalla <kayalla.p@gmail.com>
2018-11-12 00:06:42 -0200

    first commit

---
+++ wp-content/themes/marotosumahistoria/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?php wp_title(); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
+<body>
